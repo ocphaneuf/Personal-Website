c385cc96f4217c01982055b8e73e245bd311b42f
Olivier <ocphaneuf@gmail.com>
2026-01-01 15:08:41 +0000

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://ocphaneuf.github.io',
   base: '/Personal-Website',
+  trailingSlash: 'ignore',
   integrations: [tailwind()],
   build: {
     inlineStylesheets: 'auto',
